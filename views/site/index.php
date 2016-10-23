9732523328fd032a078b09cc3c5e0e266ab00bbf
Bartlomiej Augustyn <bartlomiej00augustyn@gmail.com>
2016-10-24 00:13:06 +0200

    tytuł

--- views/site/index.php
+++ views/site/index.php
@@ -8,9 +8,8 @@ $this->title = 'Portal społecznosciowy';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Witaj!</h1>
-
-        <p class="lead">obsluga menu na górze</p>
+        <h1>Witaj	<?php echo Yii::$app->user->identity->username;?>	!</h1>
+        <p class="lead">obsluga to ino menu na górze</p>
     </div>
 
     <div class="body-content">
@@ -18,16 +17,13 @@ $this->title = 'Portal społecznosciowy';
         <div class="row">
             <div class="col-lg-4">               
                 <p><a class="btn btn-default" href='index.php?r=site%2Fmessage'>Wiadomości &raquo;</a></p>
-                <p><a class="btn btn-default" href='index.php?r=site%2Fabout'>O nas &raquo;</a></p>
+                <p><a class="btn btn-default" href='index.php?r=site%2Fabout'>Moja tablica &raquo;</a></p>
                 <p><a class="btn btn-default" href='index.php?r=site%2Fcontact'>Kontakt &raquo;</a></p>
                 <p><a class="btn btn-default" href='index.php?r=site%2Flogin'>Login &raquo;</a></p>
 			</div>
             <div class="col-lg-8">
                 <h2>Heading</h2>
-
                 <p>sotry board sotry board sotry board sotry board sotry board sotry board sotry board sotry board sotry board sotry board sotry board sotry board sotry board sotry board sotry board sotry board sotry board sotry board sotry board sotry board sotry board sotry board sotry board sotry board </p>
-
-                <p><a class="btn btn-default" href='index.php?r=site%2Fmessage'>Wiadomości &raquo;</a></p>
             </div>
             
         </div>
